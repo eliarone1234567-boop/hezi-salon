5a351246f1e7cff134a06ac160310bc3e635323e
eliarone1234567-boop <eliarone1234567@gmail.com>
2026-03-29 15:57:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,29 +2,19 @@
 import express from "express";
 import cors from "cors";
 import path from "path";
-import { fileURLToPath } from "url";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// כדי לעבוד עם __dirname ב-ESM
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 app.use(cors());
 app.use(express.json());
 
-// 👇 זה החלק החשוב
-app.use(express.static(__dirname));
+// 👇 חשוב מאוד
+app.use(express.static("."));
 
 // דף הבית
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-// TEST
-app.get("/test", (req, res) => {
-  res.send("Server works 🚀");
+  res.sendFile(path.resolve("index.html"));
 });
 
 app.listen(port, () => {
